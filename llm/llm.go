68e9374d4544924dfab4a093eca0fdf2c2ecb446
zhuyst <13553371651@139.com>
2025-04-27 19:25:25 +0800

    接入百度AI搜索

--- llm/llm.go
+++ llm/llm.go
@@ -9,6 +9,7 @@ import (
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
 	log "github.com/sirupsen/logrus"
+	"strconv"
 	"strings"
 )
 
@@ -61,15 +62,24 @@ func (llm *LLM) ChatWithLLM(ctx context.Context, messages []*ChatMessage) (*LLMR
 	content := contentSb.String()
 	log.Infof("LLM content: %s", content)
 
-	chatCompletion, err := llm.client.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
+	opts := []option.RequestOption{
+		option.WithJSONSet("search_source", "baidu_search_v2"),
+		option.WithJSONSet("instruction", strings.TrimSpace(llm.cfg.Prompt)),
+		option.WithJSONSet("enable_reasoning", true),
+		option.WithJSONSet("response_format", "text"),
+		option.WithJSONSet("enable_corner_markers", false),
+	}
+
+	params := openai.ChatCompletionNewParams{
 		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-			openai.ChatCompletionMessage{Role: "system", Content: llm.cfg.Prompt},
+			//openai.ChatCompletionMessage{Role: "system", Content: llm.cfg.Prompt},
 			openai.ChatCompletionMessage{Role: "user", Content: content},
 		}),
 		Temperature: openai.Float(0.5),
 		TopP:        openai.Float(0.5),
 		Model:       openai.F(llm.cfg.Model),
-	})
+	}
+	chatCompletion, err := llm.client.Chat.Completions.New(ctx, params, opts...)
 	if err != nil {
 		log.Errorf("LLM err: %v", err)
 		return nil, err
@@ -78,12 +88,21 @@ func (llm *LLM) ChatWithLLM(ctx context.Context, messages []*ChatMessage) (*LLMR
 	res := &LLMResult{}
 	message := chatCompletion.Choices[0].Message
 	if reasoningContentI, ok := message.JSON.ExtraFields["reasoning_content"]; ok {
-		res.ReasoningContent = reasoningContentI.Raw()
+		res.ReasoningContent = convertUnicode(reasoningContentI.Raw())
 	}
 	resContent := chatCompletion.Choices[0].Message.Content
+	resContent = convertUnicode(resContent)
 	resContent = strings.ReplaceAll(resContent, "喔~", "喵 ")
 	resContent = strings.ReplaceAll(resContent, "~", " ")
 	res.Content = strings.TrimSpace(resContent)
 	log.Infof("LLM result reasoning_content: %s, content: %s", res.ReasoningContent, res.Content)
 	return res, nil
+}
+
+func convertUnicode(s string) string {
+	res, err := strconv.Unquote(strings.Replace(strconv.Quote(s), `\\u`, `\u`, -1))
+	if err != nil {
+		return s
+	}
+	return res
 }
